a2270828773f23988cd493b1ddbaa5579fe7789e
bridgetjs <bridget.sheeran@gmail.com>
2015-08-11 15:47:23 +0200

    4

--- draw.cxx
+++ draw.cxx
@@ -123,8 +123,11 @@ double CrystalBall(double* x, double* par){
     TF1* exp = new TF1("exp","exp(x)",1e-20,1e20); double A; double B;
     if (alpha < 0){
         A = pow((n/(-1*alpha)),n)*exp->Eval((-1)*alpha*alpha/2);
-        B = n/(-1*alpha) + alpha;} else {
-            A = pow((n/alpha),n)*exp->Eval((-1)*alpha*alpha/2); B = n/alpha - alpha;}
+        B = n/(-1*alpha) + alpha;
+    }
+    else {
+            A = pow((n/alpha),n)*exp->Eval((-1)*alpha*alpha/2); B = n/alpha - alpha;
+        }
     ￼double f;
     if ((xcur-mu)/sigma > (-1)*alpha)
         f = N*exp->Eval((-1)*(xcur-mu)*(xcur-mu)/ (2*sigma*sigma));
